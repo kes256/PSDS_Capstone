655a5dbb60a23b915b42812548d83f4955d7321c
Kathleen E Smith <kathleen.e.smith@nga.mil>
2020-12-04 23:10:12 -0600

    Add lat and long for Capital Cities

--- factbook.R
+++ factbook.R
@@ -1,3 +1,8 @@
+library(jsonlite)
+library(geojsonio)
+library(countrycode)
+library(tidyverse)
+
 pivoted <- 'clean_factbook.json' %>%
   read_file() %>%
   parse_json() %>%
@@ -7,19 +12,26 @@ pivoted <- 'clean_factbook.json' %>%
   pivot_wider(names_from = x3, values_from = value, id_cols = x2) %>%
   head(-3)
 
-cia_extras <- c('Kosovo' = 'XK', 'European Union' = 'EU', 'Saint Martin' = 'MAF', 'Virgin Islands' = 'VIR')  
+cia_extras <- c('Kosovo' = 'OWID_KOS', 'European Union' = 'EU', 'Saint Martin' = 'MAF', 'Virgin Islands' = 'VIR', 'World' = 'OWID_WRL')  
 pivoted$code <- countrycode(unlist(pivoted$data.name), origin='country.name', destination='iso3c', custom_match=cia_extras)
 pivoted <- pivoted %>%
   drop_na(code)
 
-names_to_keep <- read_lines('env_cols.txt')
-names_to_keep <- c(names_to_keep, read_lines('econ_cols.txt'))
-names_to_keep <- c(names_to_keep, read_lines('health_cols.txt'))
-names_to_keep <- c(names_to_keep, read_lines('edu_cols.txt'))
-names_to_keep <- c(names_to_keep, read_lines('demographic_cols.txt'))
+names_to_keep <- read_lines('columns_by_subject/env_cols.txt')
+names_to_keep <- c(names_to_keep, read_lines('columns_by_subject/econ_cols.txt'))
+names_to_keep <- c(names_to_keep, read_lines('columns_by_subject/health_cols.txt'))
+names_to_keep <- c(names_to_keep, read_lines('columns_by_subject/edu_cols.txt'))
+names_to_keep <- c(names_to_keep, read_lines('columns_by_subject/demographic_cols.txt'))
+names_to_keep <- c(names_to_keep, read_lines('columns_by_subject/geo_cols.txt'))
 
 cia_all <- pivoted[,which(colnames(pivoted) %in% names_to_keep)]
 cia_all <- unnest(cia_all, colnames(cia_all))
+
+cia_all$capital_lat <- with(cia_all, ifelse(is.na(data.government.capital.geographic_coordinates.latitude.minutes), NA, as.numeric(data.government.capital.geographic_coordinates.latitude.minutes)/60 + as.numeric(data.government.capital.geographic_coordinates.latitude.degrees)))
+cia_all <- transform(cia_all, capital_lat=ifelse(data.government.capital.geographic_coordinates.latitude.hemisphere=='S', -capital_lat, capital_lat))
+
+cia_all$capital_long <- with(cia_all, ifelse(is.na(data.government.capital.geographic_coordinates.longitude.minutes), NA, as.numeric(data.government.capital.geographic_coordinates.longitude.minutes)/60 + as.numeric(data.government.capital.geographic_coordinates.longitude.degrees)))
+cia_all <- transform(cia_all, capital_long=ifelse(data.government.capital.geographic_coordinates.longitude.hemisphere=='W', -capital_long, capital_long))
 
 world_map <- geojson_read('custom_geo.json',  what = "sp")
 spdf_fortified <- broom::tidy(world_map, region = "name")
@@ -27,4 +39,5 @@ spdf_fortified$code <- countrycode(spdf_fortified$id, origin='country.name', des
 
 spdf_cia <- inner_join(spdf_fortified, cia_all)
 
-write_csv(spdf_cia, 'map_factbook.csv')
+write_csv(cia_all, 'factbook.csv')
+#write_csv(spdf_cia, 'map_factbook.csv')
