ec468d496c5ca283fc842d848493f5c922987622
Kathleen E Smith <kathleen.e.smith@nga.mil>
2021-01-04 15:26:29 -0600

    Move plotly back to python, and patch into shiny app

--- capstone/app.R
+++ capstone/app.R
@@ -13,72 +13,47 @@ library(geojsonio)
 library(mapproj)
 library(viridis)
 library(countrycode)
-#library(devtools)
-#devtools::install_github("tylermorganwall/rayshader")
+library(plotly)
+library(reticulate)
 
-world_map <- geojson_read('custom_geo.json',  what = "sp")
-spdf_fortified <- broom::tidy(world_map, region = "name")
-spdf_fortified$code <- countrycode(spdf_fortified$id, origin='country.name', destination='iso3c', custom_match = c('Kosovo' = 'XK', 'S. Sudan' = 'SSD', 'W. Sahara' = 'ESH', 'Dem. Rep. Korea'='PRK', 'Somaliland'='SOM'))
+source_python('plots.py')
+#world_map <- geojson_read('custom_geo.json',  what = "sp")
+#spdf_fortified <- broom::tidy(world_map, region = "name")
+#spdf_fortified$code <- countrycode(spdf_fortified$id, origin='country.name', destination='iso3c', custom_match = c('Kosovo' = 'XK', 'S. Sudan' = 'SSD', 'W. Sahara' = 'ESH', 'Dem. Rep. Korea'='PRK', 'Somaliland'='SOM'))
 
 cia_all <- read_csv('factbook.csv')
 spdf_cia <- inner_join(spdf_fortified, cia_all)
 owid_data <- read.table("https://covid.ourworldindata.org/data/owid-covid-data.csv", header=T, sep=",", quote="")
+cia_owid <- left_join(cia_all, owid_data, by=c('code'='iso_code')) %>% drop_na(capital_lat, date) %>%
+    mutate_all(funs(ifelse(is.na(.), 0, .)))
 
-
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
 
     # Application title
-    titlePanel(""),
+    titlePanel("COVID reporting with supplemental data"),
 
-    # Sidebar with a slider input for number of bins 
+    # Sidebar with input 
     sidebarLayout(
         sidebarPanel(
             selectInput("col",
                         label = "Background Color",
                         choices = colnames(cia_all)
             ),
-            sliderInput("date", "Date (by week)",
-                        min = as.Date('2020-01-23'), max = lubridate::today()-1, step = 7, value = lubridate::today(), timeFormat="%F", animate = TRUE
-            ),
         ),
-
-        # Show a plot of the generated distribution
+        
+        # Show a plot 
         mainPanel(
-           plotOutput("worldMap")
+           htmlOutput("worldMap", height=1000)
         )
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output) {
 
-    output$worldMap <- renderPlot({
-        owid_date <- filter(owid_data, owid_data$date == format(input$date, "%Y-%m-%d"))
-        spdf_owid <- left_join(spdf_cia, owid_date, by=c('code'='iso_code'))
-        
-        ggplot() + 
-            geom_polygon(data = spdf_cia, aes( x = long, y = lat, group=group, fill=get(input$col)), color="white") + 
-            geom_point(data = spdf_owid, aes(capital_long, capital_lat, size=total_cases), color='red') +
-            geom_point(data = spdf_owid, aes(capital_long, capital_lat, size=new_cases), color='yellow') +
-            theme_void() + 
-            labs(
-                title = "World Coronavirus Map",
-                subtitle = "COVID reporting with supplemental data",
-                caption = "Creation: K. Smith | https://github.com/kes256/PSDS_Capstone") +
-            theme(
-                text = element_text(color = "#22211d"),
-                plot.background = element_rect(fill = "#f5f5f2", color = NA),
-                panel.background = element_rect(fill = "#f5f5f2", color = NA),
-                legend.background = element_rect(fill = "#f5f5f2", color = NA),
-                
-                plot.title = element_text(size= 22, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.4, l = 2, unit = "cm")),
-                plot.subtitle = element_text(size= 17, hjust=0.05, color = "#4e4d47", margin = margin(b = 0.1, t = 0.43, l = 2, unit = "cm")),
-                plot.caption = element_text( size=12, color = "#4e4d47", margin = margin(b = 0.3, r=0.3, unit = "cm") ),
-                legend.position = c(0.2, 0),
-            ) +
-            scale_fill_viridis(name=input$col, guide = guide_legend( keyheight = unit(3, units = "mm"), keywidth=unit(12, units = "mm"), label.position = "bottom", title.position = 'top', nrow=1) ) +
-            coord_map()
+    output$worldMap <- reactive({
+        plot_3d_data(r_to_py(cia_owid), 'new_cases', input$col, 'map.csv')
     })
 }
 
---
+++ capstone/plots.py
@@ -0,0 +1,84 @@
+import numpy as np
+import os
+import pandas as pd
+import plotly.graph_objects as go
+import plotly_express as px
+from datetime import datetime, timedelta
+
+
+def create_z_scale(df, col_name):
+    max_data = np.nanmax(df[col_name])
+    scales = [a * b for a in [1, 10, 100, 1000, 10000, 100000] for b in [1, 2, 5, 7.5]]
+    max_index = np.digitize(max_data, scales)
+    if max_data >= scales[-1]:
+        return scales[-1]
+    return scales[max_index]
+
+
+def plot_3d_data(owid_cia_df, data_to_plot, background_data, map_csv):
+    map_df = pd.read_csv(map_csv)
+    map_df['height'] = 2
+    countries = [x for _, x in map_df.groupby('group', as_index=False)]
+
+    owid_cia_df.sort_values(by=['date'], inplace=True)
+    pin_markers = go.scatter3d.Marker(size=4, symbol='circle', showscale=False)
+
+    fig = px.scatter_3d(owid_cia_df,
+                        x="capital_long", y="capital_lat", z=data_to_plot,
+                        color=data_to_plot,
+                        range_color=[0, 1],
+                        color_continuous_scale=["#DE3163", "#DE3163"],
+                        range_x=[-180, 180],
+                        range_y=[-90, 90],
+                        # range_z=[1, create_z_scale(owid_cia_df, data_to_plot)],
+                        hover_name='location',
+                        hover_data={data_to_plot: ':,.0f', 'capital_lat': False, 'capital_long': False, 'date': False},
+                        animation_frame="date",
+                        title=f'COVID-19: {data_to_plot} per country')
+    fig.update_traces(marker=pin_markers, hoverinfo='text', marker_showscale=False)
+    fig.update_traces(error_z_visible=True,
+                      error_z_type='percent',
+                      error_z_symmetric=False,
+                      error_z_value=0,
+                      error_z_valueminus=100,
+                      error_z_thickness=5,
+                      error_z_color="#DE3163")
+    fig.update_scenes(camera_eye_x=1, camera_eye_y=-2, camera_eye_z=1)
+    fig.update_scenes(yaxis_showspikes=False, yaxis_title_text='Latitude')
+    fig.update_scenes(xaxis_showspikes=False, xaxis_title_text='Longitude')
+    fig.update_scenes(zaxis_showspikes=False, zaxis_title_text='New Cases')
+    fig.update_scenes(aspectratio_x=2, aspectratio_y=2, aspectratio_z=0.5)
+    for country in countries:
+        country_color = '#DE3163'
+        fig.add_scatter3d(x=country.long, y=country.lat, z=country.height,
+                          hoverinfo='skip',
+                          mode='lines',
+                          showlegend=False,
+                          line_color='#000000')
+
+    fig.add_scatter3d(x=owid_cia_df.capital_long, y=owid_cia_df.capital_lat, z=[2] * len(owid_cia_df),
+                      hoverinfo='skip',
+                      mode='markers',
+                      # showlegend=False,
+                      marker_colorscale='viridis',
+                      marker_color=owid_cia_df[background_data],
+                      marker_cmin=min(owid_cia_df[background_data]),
+                      marker_cmax=max(owid_cia_df[background_data]),
+                      marker_size=3,
+                      marker_opacity=0.7)
+
+    return fig.to_html()
+
+
+def plot_scatter_data(owid_cia_df, data1, data2):
+    fig = px.scatter(owid_cia_df,
+                     x=data1, y=data2,
+                     range_x=[0, 3],
+                     range_y=[0, 100],
+                     color='location',
+                     hover_name='location',
+                     hover_data={'total_cases': ':,.0f', 'date': False},
+                     #animation_frame="date",
+                     title=f'COVID-19: {data1} vs. {data2}')
+    return fig.to_html()
+
